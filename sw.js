fb0fca0ef3c36f45f3693823bfa93645c9d466d2
Adriano2222222222 <138529077+Adriano2222222222@users.noreply.github.com>
2026-01-12 20:03:06 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'financas-v14';
+const CACHE_NAME = 'financas-v15';
 
 const ASSETS_TO_CACHE = [
     './',
@@ -47,6 +47,7 @@ self.addEventListener('fetch', (e) =>{
         })
     );
 });
+
 
 
 
